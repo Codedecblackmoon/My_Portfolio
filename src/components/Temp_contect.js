4bb57118cc5717ed3a29040a2176b4dc055e807b
Codacblackmoon <gideonlenge96@gmail.com>
2024-11-11 22:50:23 +0200

    starting

---
+++ src/components/Temp_contect.js
@@ -0,0 +1,18 @@
+import '../App.css';
+function Temp_contect() {
+  return (
+   <div className='nav'>
+    <nav>
+        <p>Gideonlenge96@gmail.com</p>
+        <a href="#" target="_blank" rel="noopener noreferrer">CV</a>
+    </nav>
+    <div>
+        <a href="#" target="_blank" rel="noopener noreferrer">LinkedIn</a> /
+        <a href="#" target="_blank" rel="noopener noreferrer">Instagram</a> /
+        <a href="#" target="_blank" rel="noopener noreferrer">GitHub</a>
+    </div>
+   </div>
+  );
+}
+
+export default Temp_contect;
