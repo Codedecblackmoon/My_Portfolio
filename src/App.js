5c7af1bc10972caa2dc42f47d8b5801f9370a010
Codacblackmoon <gideonlenge96@gmail.com>
2024-11-12 22:51:46 +0200

    starting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './PP2.png';
 import './App.css';
 import Contect from './components/Temp_contect';
 import React from 'react';
@@ -6,20 +6,14 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      <Contect/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Contect />
+       
+        
       </header>
     </div>
   );
