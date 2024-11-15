fa99748bbe478ae9d4b072cd122aaeb8bbfdd628
Gideon.L <gidonlenge96@gmail.com>
2024-11-15 15:41:19 +0200

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/home_page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <nav>
+        <a href="#_1">home</a>
+        <a href="#_2">about</a>
+        <a href="#_3">3</a>
+        <a href="#_4">4</a>
+        <a href="#_5">5</a>
+      </nav>
+
+      <div id="_1"><HomePage /></div>
+      <div id="_2">2</div>
+      <div id="_3"></div>
+      <div id="_4"></div>
+      <div id="_5"></div>
     </div>
   );
 }
---
+++ src/components/home_page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Rename the component to HomePage
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Hi World</h1>
+    </div>
+  );
+};
+
+export default HomePage;
