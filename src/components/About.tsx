f40f66637aeab7afd571292f6a83baa6110d2b5b
Codacblackmoon <gideonlenge96@gmail.com>
2024-11-19 23:08:42 +0200

    about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,18 @@
+import pic from '../assets/PP2.png'
+
 function About() {
     return (
         <div className="About_page">
-          {/* <img src={logo} alt="" /> */}
-          <section>
-            <h1 className="name">Gideon Lengg</h1>
-            <h3>
-              <style color="orange">“</style>There are three responses to a piece of design – yes, no, and WOW!
-              Wow is the one to aim for"
-            </h3>
-            <h4>― Milton Glaser.</h4>
-            <button>Know me</button>
-          </section>
+            
+          
+            <section>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis doloribus hic corporis, at sint quo dolorum consectetur, illum aut obcaecati eveniet, dicta itaque delectus laborum tempore. Voluptates accusamus saepe quaerat ea? Repellendus obcaecati autem recusandae vel voluptate consectetur enim veniam eveniet quidem mollitia molestiae, facilis dolor corrupti error esse sapiente unde ex. Minus quam hic debitis eius dolores blanditiis dolorem eaque in nostrum molestiae! Est nihil similique a officiis consectetur tempora saepe assumenda nesciunt culpa perferendis voluptatum, numquam ab dicta sint, ut dolor quia quas reprehenderit. Dolore molestiae id harum, eaque consectetur ut sapiente. Laboriosam incidunt quis hic in id.</p>
+                <button>cd</button>
+            </section>
+
+            <div>
+                <img src={pic} alt="" />
+            </div>
         </div>
       );
 }
