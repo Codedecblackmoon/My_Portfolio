199b5252422b226592e23ddc80d9badfe86040d8
Codacblackmoon <gideonlenge96@gmail.com>
2024-11-20 21:29:04 +0200

    about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ function About() {
           
             <section>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis doloribus hic corporis, at sint quo dolorum consectetur, illum aut obcaecati eveniet, dicta itaque delectus laborum tempore. Voluptates accusamus saepe quaerat ea? Repellendus obcaecati autem recusandae vel voluptate consectetur enim veniam eveniet quidem mollitia molestiae, facilis dolor corrupti error esse sapiente unde ex. Minus quam hic debitis eius dolores blanditiis dolorem eaque in nostrum molestiae! Est nihil similique a officiis consectetur tempora saepe assumenda nesciunt culpa perferendis voluptatum, numquam ab dicta sint, ut dolor quia quas reprehenderit. Dolore molestiae id harum, eaque consectetur ut sapiente. Laboriosam incidunt quis hic in id.</p>
-                <button>CV</button>
+                <button>Download CV</button>
             </section>
 
             <div>
