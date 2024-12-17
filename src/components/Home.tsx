8176e7fdb0465e7fd983ee6d399a8bb463a43249
Codedecblackmoon <gideonlenge96@gmail.com>
2024-12-17 19:36:27 +0200

    done with the project page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,8 +10,8 @@ function Home() {
           <section>
             <h1 className="name">Hi, I'm Gideon Lenge</h1>
             <h3>
-            <span style={{ color: "orange", fontSize: "20px", fontStyle: "bold" }}>"</span>There are three responses to a piece of design – yes, no, and WOW!
-              Wow is the one to aim for<span style={{ color: "orange", fontSize: "20px", fontStyle: "bold" }}>"</span>
+            <span style={{ color: "#008cff", fontSize: "20px", fontStyle: "bold" }}>"</span>There are three responses to a piece of design – yes, no, and WOW!
+              Wow is the one to aim for<span style={{ color: "#008cff", fontSize: "20px", fontStyle: "bold" }}>"</span>
             </h3>
             <h4>― Milton Glaser ―</h4>
             <button>Know me</button>
