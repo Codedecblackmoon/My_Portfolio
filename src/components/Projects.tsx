a81c1c2a1be2f0a2971a4b58808470384f22c082
Codedecblackmoon <gideonlenge96@gmail.com>
2024-12-17 16:51:15 +0200

    done with the project page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,52 +1,68 @@
-import pro from '../assets/pro.png'
-import stn from '../assets/path14.png'
+
+import React, { useState } from 'react';
+import pro1 from '../assets/pro.png';
+import pro2 from '../assets/pro.png'; // Adding another pro.png
+import pro3 from '../assets/pro.png'; // Adding another pro.png
+import stn from '../assets/path14.png'; // SVG-style image
 
 function Projects() {
-    return (
-        <div className="Projects_page">
-          <div className="project_grid">
-            <div className="project_card">
-              <img src={pro} />
-              <h3>Project 1</h3>
-              <div className="info">
-                <button>
-                  <i className="fa-brands fa-github"></i>
-                </button>
-                <button>
-                  <i className="fa-solid fa-globe"></i>
-                </button>
-              </div>
-            </div>
-            <div className="project_card">
-              <img src={pro} />
-              <h3>Project 2</h3>
-              <div className="info">
-                <button>
-                  <i className="fa-brands fa-github"></i>
-                </button>
-                <button>
-                  <i className="fa-solid fa-globe"></i>
-                </button>
-              </div>
-            </div>
-            <div className="project_card">
-              <img src={pro} />
-              <h3>Project 3</h3>
-              <div className="info">
-                <button>
-                  <i className="fa-brands fa-github"></i>
-                </button>
-                <button>
-                  <i className="fa-solid fa-globe"></i>
-                </button>
-              </div>
+  const slides = [
+    { id: 1, img: pro1, caption: 'Project 1' },
+    { id: 2, img: pro2, caption: 'Project 2' },
+    { id: 3, img: pro3, caption: 'Project 3' },
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
+  };
+
+  return (
+    <div className="Projects_page">
+      <div className="slideshow-container">
+
+        <a className="prev" onClick={prevSlide}>
+          &#10094;
+        </a>
+
+        {slides.map((slide, index) => (
+          <div
+            key={slide.id}
+            className={`project_card ${index === currentSlide ? 'active' : ''}`}
+            style={{ display: index === currentSlide ? 'block' : 'none' }}
+          >
+            <img src={slide.img} alt={`Slide ${index + 1}`} style={{ width: '100%' }} />
+            <h3>{slide.caption}</h3>
+            <div className="info">
+              <button>
+                <i className="fa-brands fa-github">Code</i>
+              </button>
+              <button>
+                <i className="fa-solid fa-globe">Demo</i>
+              </button>
             </div>
           </div>
-          <div id="stend">
-            <img src={stn} alt="" />
-          </div>
-        </div>
-      );
+        ))}
+
+        {/* Navigation buttons */}
+        
+        <a className="next" onClick={nextSlide}>
+          &#10095;
+        </a>
+      </div>
+
+    
+      <div id="stend" >
+        <img src={stn} alt="SVG-style side image" />
+      </div>
+    </div>
+  );
 }
 
-export default Projects 
+export default Projects;
+
