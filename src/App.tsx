bc7ebe1560da767c873c95886781056cd311bff9
Codedecblackmoon <gideonlenge96@gmail.com>
2024-12-18 20:32:44 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contect from './components/Contact'
-import AXA from './components/Expe'
-
+import AXA from './components/Expe';
+import  git from './assets/git_whait.png'
+import  intsa from './assets/insta.png'
+import  link from './assets/link.png'
 
 function App() {
   
@@ -25,7 +27,27 @@ function App() {
       <div id="_4"><Projects/></div>
       <div id="_5"><Contect/></div>
 
-      <footer><p>footer</p></footer>
+      <footer>
+        <div id="media">
+          <a href="https://www.instagram.com/gideon.l2">
+            <img src={intsa} alt="" />
+          </a>
+
+          <a href="https://www.linkedin.com/in/gideon-lenge">
+            <img src={link} alt="" />
+          </a>
+
+          <a href="https://github.com/Codedecblackmoon">
+            <img src={git} alt="" />
+          </a>
+        </div>
+
+        <div id="content">
+          <h6 id="email">gideonlenge96@gmail.com</h6>
+          <h6 id="email">+27 815 - 892 - 060</h6>
+          <h6 id="email">By Gideon.L</h6>
+        </div>
+      </footer>
     </div>
     </>
   )
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,6 +6,8 @@ function About() {
     return (
         <div className="About_page">
             <section>
+                <h2>About me</h2>
+
                 <p>I'm a dedicated and detail-oriented junior software developer with a creative approach to problem-solving, always seeking new and visually appealing ways to address technical challenges. With a foundation in both front-end and back-end development, particularly in JavaScript and Python, I enjoy crafting efficient, scalable applications that prioritize user experience. <br/> <br/> Known for my creativity, I thrive in collaborative environments where I can contribute unique solutions that enhance functionality and design.</p>
                 <a href={cv} download="Gideon_Lenge_CV.pdf">
                     <button>Download CV</button>
