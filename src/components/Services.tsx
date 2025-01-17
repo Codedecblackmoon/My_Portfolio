ec1ed8dede1f0d2d297b74f32077ca9510ddd2e6
Codedecblackmoon <gideonlenge96@gmail.com>
2025-01-17 22:29:03 +0200

    update

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -14,29 +14,29 @@ function Services() {
                     <div id='set1'>
                         <div id='s1'>
                             <img src={web} alt="" />
-                            <p><strong>Web development</strong><br />Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div id='s1'>
-                            <img src={gd} alt="" />
-                            <p><strong>Graphic Design</strong><br />Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div id='s1'>
-                            <img src={ill} alt="" />
-                            <p><strong>Illustation</strong><br />Lorem ipsum dolor sit amet.</p>
-                        </div>
-                    </div>
-                    <div id='set2'>
-                    <div id='s1'>
-                            <img src={dD} alt="" />
-                            <p><strong>3D Modaling</strong><br />Lorem ipsum dolor sit amet.</p>
+                            <p><strong>Web development</strong><br />Html, Css, Js, Typescript, Node.js</p>
                         </div>
                         <div id='s1'>
                             <img src={systems} alt="" />
-                            <p><strong>System Integrationt</strong><br />Lorem ipsum dolor sit amet.</p>
+                            <p><strong>System Integrationt</strong><br />Node.js, Python, Java</p>
                         </div>
                         <div id='s1'>
                             <img src={webapp} alt="" />
-                            <p><strong>Web Application</strong><br />Lorem ipsum dolor sit amet.</p>
+                            <p><strong>Web Application</strong><br />React, Node.js, Python</p>
+                        </div>
+                    </div>
+                    <div id='set2'>
+                        <div id='s1'>
+                            <img src={dD} alt="" />
+                            <p><strong>3D Modaling</strong><br />Blende, Maya, 3ds Max, </p>
+                        </div>
+                        <div id='s1'>
+                            <img src={gd} alt="" />
+                            <p><strong>Graphic Design</strong><br /> Adobe Photoshop,InDesign, Mockups</p>
+                        </div>
+                        <div id='s1'>
+                            <img src={ill} alt="" />
+                            <p><strong>Illustation</strong><br />Adobe Illustrator, Inkscape</p>
                         </div>
                     </div>
                 </div>
